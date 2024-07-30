eb1847dacc8bcd3b604756da61e13bcdea60f149
Jay Callicott <jrcallicott@gmail.com>
2024-07-29 21:07:40 -0500

    Updated paths

--- src/hidden/_storybook/main.js
+++ src/hidden/_storybook/main.js
@@ -5,14 +5,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  'stories': ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  'stories': ['../components/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
   'addons': [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-sass-postcss',
     '@storybook/addon-webpack5-compiler-babel',
     '@chromatic-com/storybook',
-    '@storybook/addon-themes'
+    '@storybook/addon-themes',
+    '@storybook/addon-mdx-gfm'
   ],
   'framework': {
     name: '@storybook/html-webpack5',
