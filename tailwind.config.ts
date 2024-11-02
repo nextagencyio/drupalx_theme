bc362820bbd82524e03faa8958c5b82381517e7d
Jay Callicott <jrcallicott@gmail.com>
2024-11-02 18:20:04 -0500

    Theme updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+			},
 			typography: (theme: any) => ({
 				DEFAULT: {
 					css: {
