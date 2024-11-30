58ed9600376cb318e5a68c105e039923297c2579
Jay Callicott <jrcallicott@gmail.com>
2024-11-30 09:18:49 -0600

    Updated gallery

--- components/form/form.stories.js
+++
@@ -1,8 +0,0 @@
-import FormTemplate from './form.twig';
-
-export default {
-  title: 'Editorial/Form',
-};
-
-export const Form = FormTemplate.bind({});
-Form.args = {};
--- components/gallery/gallery.stories.js
+++ components/gallery/gallery.stories.js
@@ -1,101 +1,91 @@
-import GalleryLightboxTemplate from './gallery.twig';
+import galleryLightboxTemplate from './gallery.twig';
+import './gallery.behavior.js';
 
+/**
+ * Meta configuration for the Gallery component.
+ * Aligns with React Storybook by specifying title, component, and argTypes.
+ */
 export default {
   title: 'Editorial/Gallery',
+  component: galleryLightboxTemplate,
   argTypes: {
     section_title: {
       description: 'The title of the gallery lightbox component',
-      control: 'text'
+      control: 'text',
+      defaultValue: 'Optional Title',
     },
     intro_text: {
       description: 'Optional intro text for the gallery lightbox component',
-      control: 'text'
+      control: 'text',
+      defaultValue:
+        '<p>Optional summary text, turpis at luctus finibus, erat lectus convallis velit, at sodales purus lacus quis magna. Curabitur imperdiet sapien libero, fringilla ullamcorper nibh ullamcorper vitae. Proin sed luctus augue.</p>',
     },
     gallery_items: {
       description: 'Define the array of gallery lightbox items',
-      control: 'array',
-      type: {
-        required: true
-      }
+      control: 'object',
+      defaultValue: [
+        {
+          id: 'exampleLightbox1',
+          media:
+            "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 1' />",
+          media_thumb:
+            "<img src='./images/card.webp' alt='Gallery 1' />",
+          media_description: 'Gallery 1 here!',
+        },
+      ],
     },
     modifier: {
       description: 'The modifier class to apply to the gallery lightbox component',
-      control: 'text'
-    }
-  }
+      control: 'text',
+      defaultValue: 'p-2',
+    },
+  },
 };
 
-export const Gallery = GalleryLightboxTemplate.bind({});
+const template = (args) => galleryLightboxTemplate(args);
 
-Gallery.args = {
-  section_title: 'Optional Title',
-  modifier: 'p-2',
+/**
+ * Default Gallery Story
+ * Mirrors the React Storybook's 'Default' story structure.
+ */
+export const Default = template.bind({});
+Default.args = {
+  section_title: 'Gallery Title',
   intro_text:
-    '<p>Optional summary text, turpis at luctus finibus, erat lectus convallis velit, at sodales purus lacus quis magna. Curabitur imperdiet sapien libero, fringilla ullamcorper nibh ullamcorper vitae. Proin sed luctus augue.</p>',
+    '<p>This is a sample summary for the gallery.</p>',
+  modifier: 'p-2',
   gallery_items: [
     {
       id: 'exampleLightbox1',
       media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 1' />",
+        "<img src='./images/card.webp' width='100%' alt='Gallery 1' />",
       media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 1' />",
-      media_description: 'Gallery 1 here!'
+        "<img src='./images/card.webp' alt='Gallery 1' />",
+      media_description: 'Gallery 1 here!',
     },
     {
       id: 'exampleLightbox2',
       media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 2' />",
+        "<img src='./images/card.webp' width='100%' alt='Gallery 2' />",
       media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 2' />",
-      media_description: 'Gallery 2 here!'
+        "<img src='./images/card.webp' alt='Gallery 2' />",
+      media_description: 'Gallery 2 here!',
     },
     {
       id: 'exampleLightbox3',
       media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 3' />",
+        "<img src='./images/card.webp' width='100%' alt='Gallery 3' />",
       media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 3' />",
-      media_description: 'Gallery 3 here!'
+        "<img src='./images/card.webp' alt='Gallery 2' />",
+      media_description: 'Gallery 2 here!',
     },
     {
       id: 'exampleLightbox4',
       media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 4' />",
+        "<img src='./images/card.webp' width='100%' alt='Gallery 4' />",
       media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 4' />",
-      media_description: 'Gallery 4 here!'
+        "<img src='./images/card.webp' alt='Gallery 2' />",
+      media_description: 'Gallery 2 here!',
     },
-    {
-      id: 'exampleLightbox5',
-      media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 5' />",
-      media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 5' />",
-      media_description: 'Gallery 5 here!'
-    },
-    {
-      id: 'exampleLightbox6',
-      media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 6' />",
-      media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 6' />",
-      media_description: 'Gallery 6 here!'
-    },
-    {
-      id: 'exampleLightbox7',
-      media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 7' />",
-      media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 7' />",
-      media_description: 'Gallery 7 here!'
-    },
-    {
-      id: 'exampleLightbox8',
-      media:
-        "<img src='./images/card.webp' width='100%' class='card-img-top' alt='Gallery 8' />",
-      media_thumb:
-        "<img src='./images/card.webp' class='img-fluid' alt='Gallery 8' />",
-      media_description: 'Gallery 8 here!'
-    }
-  ]
+  ],
 };
