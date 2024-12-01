999801e2578f1ea1ffd5848628ef89e53c1d02a9
Jay Callicott <jrcallicott@gmail.com>
2024-11-30 19:36:32 -0600

    Updated text

--- components/text/text.stories.js
+++ components/text/text.stories.js
@@ -3,30 +3,80 @@ import TextBlockTemplate from './text.twig';
 export default {
   title: 'Editorial/Text Block',
   argTypes: {
-    body_text: {
-      description: 'Define the Text Block copy.',
-      control: 'text'
+    eyebrow: {
+      control: 'text',
+      description: 'Optional eyebrow text displayed above the title'
+    },
+    title: {
+      control: 'text',
+      description: 'Main title of the text block'
     },
     heading: {
-      description: 'Define the Text Block heading.',
-      control: 'text'
+      control: 'object',
+      description: 'Optional heading object that overrides the title'
     },
-    modifier: {
-      description: 'Define the Text Block modifier class.',
-      control: 'text'
+    body: {
+      control: 'text',
+      description: 'Main content of the text block'
+    },
+    link: {
+      control: 'object',
+      description: 'Primary button/link configuration'
+    },
+    link2: {
+      control: 'object',
+      description: 'Secondary button/link configuration'
+    },
+    text_layout: {
+      control: 'select',
+      options: ['default', 'centered', 'buttons-right'],
+      description: 'Layout configuration for the text block'
+    },
+    className: {
+      control: 'text',
+      description: 'Additional CSS classes for the container'
     }
   }
 };
 
-export const TextBlock = TextBlockTemplate.bind({});
-
-TextBlock.args = {
-  modifier: 'col-10 p-4',
-  heading: {
-    title: 'Title Lorem Ipsum Dolor',
-    level: '2',
-    modifier: 'mb-2 display-3'
+export const Default = TextBlockTemplate.bind({});
+Default.args = {
+  eyebrow: 'Test eyebrow',
+  title: 'Title Lorem Ipsum Dolor',
+  body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mauris mi, aliquam in orci at, finibus malesuada elit. Vivamus ex ante, imperdiet nec odio ac, sollicitudin fermentum velit.</p>',
+  link: {
+    url: '#',
+    text: 'Read more'
   },
-  body_text:
-    '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mauris mi, aliquam in orci at, finibus malesuada elit. Vivamus ex ante, imperdiet nec odio ac, sollicitudin fermentum velit. Nunc vestibulum massa est, eu auctor libero pellentesque eu. Cras id augue a lacus imperdiet convallis dictum vel diam. Fusce ut ex ac sem condimentum ornare. Nam at rutrum enim. Quisque convallis augue in nisi interdum, ac suscipit elit sollicitudin.</p><p>Nulla velit purus, varius quis velit aliquet, lobortis venenatis mauris. In non ligula eget ex semper pulvinar. Aliquam aliquet hendrerit auctor. Duis bibendum placerat risus, non commodo magna ornare id. Morbi eget nulla hendrerit, molestie nisl eget, semper odio. Mauris mollis risus sit amet ligula mattis vehicula. Donec maximus condimentum lacus, quis porttitor risus fermentum id. Duis semper neque in interdum pulvinar. Nam venenatis interdum libero. Proin dictum blandit ante sollicitudin laoreet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas in pharetra ligula.</p>'
+  link2: {
+    url: '#',
+    text: 'Learn more'
+  },
+  text_layout: 'default',
+  className: ''
+};
+
+export const Centered = TextBlockTemplate.bind({});
+Centered.args = {
+  ...Default.args,
+  text_layout: 'centered'
+};
+
+export const ButtonsRight = TextBlockTemplate.bind({});
+ButtonsRight.args = {
+  ...Default.args,
+  text_layout: 'buttons-right'
+};
+
+export const NoLinks = TextBlockTemplate.bind({});
+NoLinks.args = {
+  ...Default.args,
+  link: undefined,
+  link2: undefined
+};
+
+export const SingleLink = TextBlockTemplate.bind({});
+SingleLink.args = {
+  ...Default.args,
+  link2: undefined
 };
