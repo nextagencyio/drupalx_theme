26eb7f2f0d5edbc75300a89308615fc2636afbca
Jay Callicott <jrcallicott@gmail.com>
2024-12-02 07:04:29 -0600

    Updated gallery cypress

--- components/gallery/gallery.cy.js
+++ components/gallery/gallery.cy.js
@@ -1,32 +1,45 @@
 describe('Gallery Lightbox Component', () => {
   beforeEach(() => {
-    cy.visit('/iframe.html?args=&id=editorial-gallery--gallery&viewMode=story');
+    cy.visit('/iframe.html?args=&id=editorial-gallery--default');
   });
 
   it('should display the gallery section with title and intro text', () => {
-    cy.get('.mb-4.text-md-center').should('contain.text', 'Optional Title');
-    cy.get('.text-md-center.pb-3.col-md-8').should('contain.html', 'Optional summary text');
+    cy.get('.text-center .text-3xl.font-bold').should('contain.text', 'Gallery Title');
+    cy.get('.text-center.pb-3.md\\:w-2\\/3').should('contain.html', 'This is a sample summary for the gallery');
   });
 
   it('should display all gallery items', () => {
-    cy.get('.row > .col-6.col-md-3.mb-3').should('have.length', 8);
+    cy.get('.grid.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-4 > div').should('have.length', 4);
   });
 
   it('should open and close the lightbox modal', () => {
-    cy.get('.col-6.col-md-3.mb-3').first().find('a').click();
-    cy.get('.modal').first().should('have.class', 'show');
+    // First get the grid item and click it directly
+    cy.get('.grid.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-4 > div')
+      .first()
+      .click();
 
-    cy.get('.modal').first().find('button.btn-secondary').click();
+    // Verify modal appears
+    cy.get('.modal:not(.hidden)').should('exist').and('be.visible');
+
+    // Close modal using close button
+    cy.get('.modal:not(.hidden) button.modal-close').click();
+
+    // Verify modal is closed/hidden
+    cy.get('.modal:not(.hidden)').should('not.exist');
   });
 
   it('should display correct media and description in lightbox modal', () => {
-    const firstItem = cy.get('.col-6.col-md-3.mb-3').first();
-    firstItem.find('a').click();
+    // Click the first gallery item
+    cy.get('.grid.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-4 > div')
+      .first()
+      .click();
 
-    cy.get('.modal').first().find('.modal-title').should('contain.text', 'Gallery 1 here!');
-    cy.get('.modal').first().find('.gallery-body img').should('have.attr', 'alt', 'Gallery 1');
+    // Check modal content
+    cy.get('.modal:not(.hidden) h2').should('contain.text', 'Gallery 1 here!');
+    cy.get('.modal:not(.hidden) img').should('have.attr', 'alt', 'Gallery 1');
 
-    cy.get('.modal').first().find('button.btn-secondary').click();
+    // Close modal
+    cy.get('.modal:not(.hidden) button.modal-close').click();
   });
 
   it('should be responsive on various screen sizes', () => {
@@ -35,7 +48,14 @@ describe('Gallery Lightbox Component', () => {
     sizes.forEach((size) => {
       cy.viewport(size);
 
-      cy.get('.row > .col-6.col-md-3.mb-3').should('have.length', 8);
+      // Check if grid layout changes appropriately
+      if (size === 'iphone-6') {
+        cy.get('.grid').should('have.class', 'grid-cols-1');
+      } else {
+        cy.get('.grid').should('have.class', 'sm:grid-cols-2');
+      }
+
+      cy.get('.grid.grid-cols-1.sm\\:grid-cols-2.lg\\:grid-cols-4 > div').should('have.length', 4);
     });
   });
 });
